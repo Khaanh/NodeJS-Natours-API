d753aefce33c036e3be2ef2a4632c34fd7656936
Khanh <Khanh13Nguyen13@gmail.com>
2024-09-19 13:51:33 +0200

    finished router

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // 1) Middleware - function that modify the incoming data (between req and res)
-app.use(morgan("dev"));
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
 	console.log("Hello from the middleware");
@@ -18,12 +22,8 @@ app.use((req, res, next) => {
 	next();
 });
 
-// 3) ROUTES
+// 2) ROUTES
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
 
-// 4) START SERVER
-const PORT = 3000;
-app.listen(PORT, () => {
-	console.log(`App running on port ${PORT}...`);
-});
+module.exports = app;
